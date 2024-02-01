eb70ec2b063fa4d3d260cc8b335ba2a6a83b1768
ayatsakalla <ayat.sakalla@outlook.com>
2024-02-01 01:39:02 -0500

    gonna lose my mind nothing is working.

---
+++ script.js
@@ -0,0 +1,29 @@
+function year1556() {
+    document.getElementById('title').innerText = "1556: World's Deadliest Earthquake";
+    // document.getElementById('image').src = "https://picsum.photos/200";
+    // document.getElementById('iframe').src = "https://picsum.photos/200";
+}
+
+function year1832() {
+    document.getElementById('title').innerText = "1832: Édouard Manet";
+    // document.getElementById('image').src = https://picsum.photos/200;
+    // document.getElementById('iframe').src = //insert iframe src here;
+}
+
+function year1984() {
+    document.getElementById('title').innerText = "1984: Arjen Robben";
+    // document.getElementById('image').src = //insert img src here;
+    // document.getElementById('iframe').src = //insert iframe src here;
+}
+
+function year1986() {
+    document.getElementById('title').innerText = "1986: The Rock and Roll Hall of Fame";
+    // document.getElementById('image').src = //insert img src here;
+    // document.getElementById('iframe').src = //insert iframe src here;
+}
+
+function year2005() {
+    document.getElementById('title').innerText = "2005: Johhny Carson";
+    // document.getElementById('image').src = //insert img src here;
+    // document.getElementById('iframe').src = //insert iframe src here;
+}
