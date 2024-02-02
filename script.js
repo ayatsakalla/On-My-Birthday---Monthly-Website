83e75e75a9c2ba93cbc274a1531277d8430c1e7c
ayatsakalla <ayat.sakalla@outlook.com>
2024-02-01 22:31:09 -0500

    DONEZOOO I HATE RESPONSIVENESS AND BOOTSTRAP AND JS WOOOO

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 function year1556() {
-    document.getElementById('title').innerText = "1556: World's Deadliest Earthquake";
-    // document.getElementById('image').src = "https://picsum.photos/200";
-    // document.getElementById('iframe').src = "https://picsum.photos/200";
+    document.getElementById('date').innerText = "January 23, 1556";
+    document.getElementById('title').innerText = "World's Deadliest Earthquake";
+    document.getElementById('content').innerText = "The deadliest earthquake on record occurred in 1556, claiming the lives of approximately 830,000 people. This devastating event struck the Shaanxi province in China, causing widespread destruction. The earthquake's magnitude is estimated to have been around 8.0, and its impact on the densely populated region led to one of the highest recorded death tolls in history. The catastrophic event left a lasting mark on seismic studies and disaster preparedness.";
+    document.getElementById('image').src = 'js/js-earthquake.webp';
+    document.getElementById('iframe').src = 'https://www.youtube.com/embed/1e4mXLhRNUg?si=nS9tYIH5zUZ0NQLz';
 }
 
 function year1832() {
-    document.getElementById('title').innerText = "1832: Edouard Manet";
-    // document.getElementById('image').src = https://picsum.photos/200;
-    // document.getElementById('iframe').src = //insert iframe src here;
+    document.getElementById('date').innerText = "January 23, 1832";
+    document.getElementById('title').innerText = "Edouard Manet";
+    document.getElementById('content').innerText = "Edouard Manet, a prominent French painter, was born on January 23, 1832, in Paris, France. He is considered a pivotal figure in the transition from Realism to Impressionism in the world of art. Manet's works, including 'Olympia' and 'Luncheon on the Grass,' challenged conventional artistic norms and sparked controversy in the 19th-century Parisian art scene. His innovative approach and unique style laid the groundwork for modern art movements.";
+    document.getElementById('image').src = 'js/js-manet.webp';
+    document.getElementById('iframe').src = 'https://www.youtube.com/embed/bAmbF0z0YZo?si=c_5JUYA_3sRZOdfr';
 }
 
 function year1984() {
-    document.getElementById('title').innerText = "1984: Arjen Robben";
-    // document.getElementById('image').src = //insert img src here;
-    // document.getElementById('iframe').src = //insert iframe src here;
+    document.getElementById('date').innerText = "January 23, 1984";
+    document.getElementById('title').innerText = "Arjen Robben";
+    document.getElementById('content').innerText = "Arjen Robben, a retired Dutch footballer, was born on January 23. He enjoyed a successful career as a winger, known for his speed, skill, and precise left foot. Robben played for several top clubs, including FC Bayern Munich and Chelsea, winning numerous domestic and international titles. Renowned for his trademark cutting inside from the right wing, he left a lasting impact on the football world and was a key figure for the Dutch national team in multiple FIFA World Cup tournaments.";
+    document.getElementById('image').src = 'js/js-robben.jpg';
+    document.getElementById('iframe').src = 'https://www.youtube.com/embed/BimVTO7qH-s?si=BjBKy1NMr3f-Gp78';
 }
 
 function year1986() {
-    document.getElementById('title').innerText = "1986: The Rock and Roll Hall of Fame";
-    // document.getElementById('image').src = //insert img src here;
-    // document.getElementById('iframe').src = //insert iframe src here;
+    document.getElementById('date').innerText = "January 23, 1986";
+    document.getElementById('title').innerText = "The Rock and Roll Hall of Fame";
+    document.getElementById('content').innerText = "The Rock and Roll Hall of Fame in Cleveland inducted its inaugural class on January 23, 1986. This historic induction included influential figures in the genre, such as Chuck Berry, often hailed as a pioneer of rock and roll. Fats Domino, known for his rhythm and blues contributions, and the iconic Elvis Presley, the 'King of Rock and Roll,' were also honored in this first class. The induction marked a significant moment in recognizing and celebrating the impact of these artists on the evolution of rock and roll music.";
+    document.getElementById('image').src = 'js/rock-roll-js.avif';
+    document.getElementById('iframe').src = 'https://www.youtube.com/embed/vLtIkfEYymE?si=5chBzWjxPl1cDAfS';
 }
 
 function year2005() {
-    document.getElementById('title').innerText = "2005: Johhny Carson";
-    // document.getElementById('image').src = //insert img src here;
-    // document.getElementById('iframe').src = //insert iframe src here;
+    document.getElementById('date').innerText = "January 23, 2005";
+    document.getElementById('title').innerText = "Johhny Carson";
+    document.getElementById('content').innerText = "Johnny Carson, the legendary American television host, passed away on January 23, 2005, at the age of 79. Carson was best known for hosting 'The Tonight Show Starring Johnny Carson' for three decades, from 1962 to 1992, making him an iconic figure in late-night television. With his wit, charm, and comedic talents, Carson became a beloved household name and influenced generations of comedians. His impact on the entertainment industry and late-night television remains enduring, solidifying his legacy as one of the greatest hosts in television history.";
+    document.getElementById('image').src = 'js/js-carson.jpg';
+    document.getElementById('iframe').src = 'https://www.youtube.com/embed/UW0263yqJHI?si=35IrDiYDqVUA8qqg';
 }
